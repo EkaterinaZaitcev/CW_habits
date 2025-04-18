28484481b6fe00c89104b7111c29e302b8a6f501
EkaterinaZaitcev <katyat1@mail.ru>
2025-04-18 23:46:28 +1000

    Созданы модель привычек, юзера, CruD user

--- users/models.py
+++ users/models.py
@@ -1,3 +1,22 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+class User(AbstractUser):
+    """Клас пользователь"""
+    username = None
+    email = models.EmailField(unique=True, verbose_name="Почта")
+    phone = models.CharField(
+        max_length=15, blank=True, null=True, verbose_name="Телефон"
+    )
+    city = models.CharField(max_length=30, blank=True, null=True, verbose_name="Город")
+    avatar = models.ImageField(
+        upload_to="users/avatar", blank=True, null=True, verbose_name="Аватар"
+    )
+    tg_chat_id = models.CharField(max_length=50, blank=True, null=True, verbose_name="Telegram chat_id")
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    class Meta:
+        verbose_name = "Пользователь"
+        verbose_name_plural = "Пользователи"
