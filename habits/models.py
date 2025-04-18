379531d464c64021d32a4d0e153f854da255c190
EkaterinaZaitcev <katyat1@mail.ru>
2025-04-18 23:46:28 +1000

    Созданы модель привычек, юзера, CruD user

--- habits/models.py
+++ habits/models.py
@@ -1,3 +1,66 @@
 from django.db import models
 
-# Create your models here.
+from users.models import User
+
+
+class Habits(models.Model):
+    """Класс привычек"""
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+        verbose_name="Создатель привычки",
+        help_text="Укажите создателя привычки."
+    )
+    location = models.CharField(
+        max_length=250,
+        default="Дома",
+        verbose_name="Место",
+        help_text="Место, в котором необходимо выполнять привычку.",
+    )
+    time = models.TimeField(
+        verbose_name="Время",
+        help_text="Время, когда необходимо выполнять привычку."
+    )
+    action = models.CharField(
+        max_length=255,
+        verbose_name="Действие",
+        help_text="Действие, которое представляет собой привычка.",
+    )
+    pleasant_habit = models.BooleanField(
+        default=False,
+        verbose_name="Полезная привычка",
+        help_text="Привычка, которую можно привязать к выполнению полезной привычки."
+    )
+    related_habit = models.ForeignKey(
+        "self",
+        on_delete=models.SET_NULL,
+        verbose_name="Связанная привычка",
+        help_text="Привычка, которая связана с другой привычкой, важно указывать для полезных привычек, но не для приятных."
+    )
+    periodicity = models.PositiveIntegerField(
+        verbose_name = "Периодичность",
+        help_text = "Периодичность выполнения привычки для напоминания в днях."
+    )
+    reward = models.CharField(
+        max_length=255,
+        verbose_name="Вознаграждение",
+        help_text="Чем пользователь должен себя вознаградить после выполнения."
+    )
+    time_to_complete = models.DurationField(
+        verbose_name="Время на выполнение",
+        help_text="Время, которое предположительно потратит пользователь на выполнение привычки."
+    )
+    publicity_sign = models.BooleanField(
+        default=True,
+        verbose_name="Признак публичности",
+        help_text="Привычки можно публиковать в общий доступ, чтобы другие пользователи могли брать в пример чужие привычки."
+    )
+
+    def __str__(self):
+        return f'{self.user} - {self.action}'
+
+    class Meta:
+        verbose_name = "Привычка",
+        verbose_name_plural = "Привычки"
--- users/views.py
+++ users/views.py
@@ -1,3 +1,36 @@
-from django.shortcuts import render
+from rest_framework.generics import CreateAPIView, ListAPIView, UpdateAPIView, RetrieveAPIView
+from rest_framework.permissions import AllowAny, IsAuthenticated
 
-# Create your views here.
+from users.models import User
+from users.serializers import UserSerializer
+
+
+class UserCreateAPIView(CreateAPIView):
+    """ Регистрация нового пользователя """
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+    permission_classes = [AllowAny]
+
+    def perform_create(self, serializer):
+        user = serializer.save()
+        user.set_password(serializer.validated_data["password"])
+        user.is_active = True
+        user.save()
+
+class UserListAPIView(ListAPIView):
+    """ Для просмотра списка профилей """
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+    permission_classes = [IsAuthenticated]
+
+class UserUpdateAPIView(UpdateAPIView):
+    """ Изменение профиля пользователя """
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+    permission_classes = [IsAuthenticated]
+
+class UserRetrieveAPIView(RetrieveAPIView):
+    """ Просмотр пользователя по ID """
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+    permission_classes = [IsAuthenticated]
