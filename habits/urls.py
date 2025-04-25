2291835d930edd700982aba0e86d70479ae0451f
EkaterinaZaitcev <katyat1@mail.ru>
2025-04-25 16:08:50 +1000

    Не создает привычки

--- habits/urls.py
+++ habits/urls.py
@@ -1,5 +1,4 @@
 from django.urls import path
-from . import views
 
 from habits.apps import HabitsConfig
 from habits.views import (HabitsCreateAPIview, HabitsDeleteAPIview,
@@ -9,10 +8,10 @@ from habits.views import (HabitsCreateAPIview, HabitsDeleteAPIview,
 app_name = HabitsConfig.name
 
 urlpatterns = [
-    path('list/', views.HabitsListAPIview.as_view(), name="habits_list"),
-    path('create/', views.HabitsCreateAPIview.as_view(), name="habits_create"),
-    path('update/<int:pk>/', views.HabitsUpdateAPIview.as_view(), name="habits_update"),
-    path('retrieve/<int:pk>/', views.HabitsRetrieveAPIview.as_view(), name="habits_retrieve"),
-    path('delete/<int:pk>/', views.HabitsDeleteAPIview.as_view(), name="habits_delete"),
-    path('publishing/', views.HabitsPublishingListAPIview.as_view(), name="publishing_list"),
+    path('list/', HabitsListAPIview.as_view(), name="habits_list"),
+    path('create/', HabitsCreateAPIview.as_view(), name="habits_create"),
+    path('update/<int:pk>/', HabitsUpdateAPIview.as_view(), name="habits_update"),
+    path('retrieve/<int:pk>/', HabitsRetrieveAPIview.as_view(), name="habits_retrieve"),
+    path('delete/<int:pk>/', HabitsDeleteAPIview.as_view(), name="habits_delete"),
+    path('publishing/', HabitsPublishingListAPIview.as_view(), name="publishing_list"),
 ]
