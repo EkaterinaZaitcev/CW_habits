856f01e09fcfc2ffdcf378e69fe5830eb7552d9c
EkaterinaZaitcev <katyat1@mail.ru>
2025-04-29 01:45:52 +1000

    Подключение Телеграмм не работает

--- habits/tasks.py
+++ habits/tasks.py
@@ -1,17 +1,12 @@
 from datetime import datetime, timedelta, timezone
-
 from celery import shared_task
-
-from config import settings
 from habits.models import Habit
-from users.models import User
 from habits.services import send_telegram_message
 
 @shared_task
 def send_reminder():
     """Напоминание о привычке пользователю в назначенное время"""
-    habits = Habit.objects.all()
-    for habit in habits:
+    for habit in Habit.objects.all():
         if habit.time <= timezone.now().time():
             tg_chat_id = habit.user.tg_chat_id
             message = f"Пора выполнять: {habit.action}"
