de5d800c4ecd6e44c23c10adee386133a64a6b25
EkaterinaZaitcev <katyat1@mail.ru>
2025-04-30 23:13:55 +1000

    Подключение работает

--- habits/tasks.py
+++ habits/tasks.py
@@ -1,16 +1,27 @@
+import json
+
 from datetime import *
-from django.utils import timezone
 from celery import shared_task
+
+import users
 from habits.models import Habit
 from habits.services import send_telegram_message
 
 @shared_task
 def send_reminder():
     """Напоминание о привычке пользователю в назначенное время"""
+    try:
+        now = datetime.now()
+        time_threshold = now - timedelta(seconds=40)
 
-    habits = Habit.objects.all()
-    for habit in habits:
-        if habit.time <= timezone.now().time():
-            tg_chat_id = habit.user.tg_chat_id
-            message = f"Пора выполнять: {habit.action}"
-            send_telegram_message(tg_chat_id, message)
+        habits_to_remind = Habit.objects.filter(
+            time__lt=now.time(), time__gt=time_threshold.time()
+        )
+        for habit in habits_to_remind:
+            chat_id = users.tg_chat_id
+            if chat_id:
+                message = f"Я буду {habit.action} в {habit.time} в {habit.place}"
+                send_telegram_message(chat_id, message)
+                print("Сообщение отправлено")
+    except Habit.DoesNotExist:
+        pass
